58ab70175b848d5f459cec1682ed077a3e3c4df1
frrst-ian <frrstxd@proton.me>
2024-07-12 14:52:56 +0800

    Add svg icon to contact page

--- src/contact.js
+++ src/contact.js
@@ -1,19 +1,37 @@
-const contactPage = ()  => {
+const contactPage = () => {
     const contactTitle = document.createElement('p');
-    contactTitle.setAttribute('id' , 'contact-title');
-    contactTitle.textContent= 'Contact Us';
+    contactTitle.setAttribute('id', 'contact-title');
+    contactTitle.textContent = 'Contact Us';
 
     const contactDiv = document.createElement('div');
-    contactDiv.setAttribute('id','contact-div');
+    contactDiv.setAttribute('id', 'contact-div');
 
     const contactContentTitle = document.createElement('p');
-    contactContentTitle.setAttribute('id' , 'contact-content-title');
+    contactContentTitle.setAttribute('id', 'contact-content-title');
     contactContentTitle.textContent = 'General Manager';
 
-    const contactContent =document.createElement('p');
-    contactContent.setAttribute('id' , 'contact-content');
-;
-    contactContent.innerHTML = '09222555100' + '<br>' + 'delusionall@imreal.com'
+    const contactContent = document.createElement('p');
+    contactContent.setAttribute('id', 'contact-content');
+    ;
+    contactContent.innerHTML = '09222555100' + '<br>' + 'delusional@imreal.com'
+
+    const contactSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    contactSVG.setAttribute('id' , 'contact-svg')
+    contactSVG.setAttribute('fill', '#000000');
+    contactSVG.setAttribute('width', '200');
+    contactSVG.setAttribute('height', '200');
+    contactSVG.setAttribute('viewBox', '0 0 32 32');
+    contactSVG.setAttribute('style', "fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;");
+    contactSVG.setAttribute('version', '1.1');
+    contactSVG.setAttribute('xml:space', 'preserve');
+    document.getElementById('content').appendChild(contactSVG)
+
+    contactSVG.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:serif", "http://www.serif.com/");
+    contactSVG.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+
+    const path = document.createElementNS('http://www.w3.org/2000/svg' , 'path');
+    path.setAttribute('d' , "M16,2c-7.727,0 -14,6.273 -14,14c0,7.727 6.273,14 14,14c7.727,0 14,-6.273 14,-14c-0,-7.727 -6.273,-14 -14,-14Zm-6.729,10.345l0.15,0.259l6.23,10.792l0.866,0.5c0,0 -0.861,-0.493 -0.861,-0.493l0.15,0.26c0.475,0.824 1.378,1.386 2.555,1.483c0.993,0.082 2.117,-0.184 3.119,-0.763c1.121,-0.647 2.118,-1.223 2.118,-1.223c0.478,-0.276 0.642,-0.887 0.366,-1.366l-2.077,-3.597c-0.276,-0.478 -0.888,-0.642 -1.366,-0.366l-0.866,0.5c0,-0 -3.846,-6.662 -3.846,-6.662c-0,0 0.866,-0.5 0.866,-0.5c0.478,-0.276 0.642,-0.888 0.366,-1.366l-2.077,-3.597c-0.276,-0.479 -0.888,-0.642 -1.366,-0.366c0,-0 -0.997,0.575 -2.118,1.223c-1.002,0.578 -1.795,1.419 -2.22,2.319c-0.505,1.068 -0.469,2.131 0.006,2.955l0.005,0.008Zm8.117,10.058l-6.39,-11.066c-0.177,-0.307 -0.118,-0.702 0.07,-1.1c0.267,-0.566 0.782,-1.079 1.412,-1.442c0,-0 1.252,-0.723 1.252,-0.723c0,-0 1.077,1.865 1.077,1.865c-0,0 -0.866,0.5 -0.866,0.5c-0.478,0.276 -0.642,0.888 -0.366,1.366l4.846,8.394c0.276,0.478 0.888,0.642 1.366,0.366l0.866,-0.5c0,-0 1.077,1.865 1.077,1.865c0,0 -1.252,0.723 -1.252,0.723c-0.63,0.364 -1.331,0.553 -1.955,0.502c-0.43,-0.035 -0.794,-0.176 -0.976,-0.47l-0.161,-0.28Z")
+    contactSVG.appendChild(path);
 
     document.getElementById('content').appendChild(contactTitle);
     document.getElementById('content').appendChild(contactDiv);
@@ -22,4 +40,4 @@ const contactPage = ()  => {
 
 
 }
-export{contactPage};
+export { contactPage };
--- src/home.js
+++ src/home.js
@@ -24,26 +24,20 @@ const homePage = () => {
 
   para.setAttribute('id', 'para');
   hoursOpen.setAttribute('id', 'desc-para');
-  location.setAttribute('id','location-text');
+  location.setAttribute('id', 'location-text');
   locationTitle.setAttribute('id', 'location-title');
 
-  //SVG 
-//   <?xml version="1.0" encoding="UTF-8"?>
-// <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
-// <svg fill="#000000" width="800px" height="800px" viewBox="0 0 15 15" id="restaurant-noodle" xmlns="http://www.w3.org/2000/svg">
-//   <path d="M4.457,11.9892,1,8V7H14V8l-3.4961,3.9891ZM3.9882,2.5a.5.5,0,0,0-1,0v.5671l-1.7969.3675a.25.25,0,1,0,.094.4911l1.7029-.2776v.5662l-1.75.0357a.25.25,0,0,0,0,.5l1.75.0357V5.998h1Zm9.5,1.5-7.5.2625V2.9951l7.594-1.0737a.5.5,0,0,0-.1881-.9822L5.9792,2.4555a.4963.4963,0,0,0-.991.0445v.2276l-.493.1009V3.18l.493-.08V4.2974l-.493.01v.4608L13.4882,5a.5.5,0,0,0,0-1ZM10,13H5v.5757h5Z"/>
-// </svg>
-const logoSVG = document.createElementNS('http://www.w3.org/2000/svg','svg');
-logoSVG.setAttribute('width' , '800');
-logoSVG.setAttribute('height' , '800');
-logoSVG.setAttribute('viewBox' ,'0 0 15 15');
-logoSVG.setAttribute('id','logo');
-document.getElementById('content').appendChild(logoSVG);
+  const logoSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  logoSVG.setAttribute('width', '1600px');
+  logoSVG.setAttribute('height', '1600px');
+  logoSVG.setAttribute('viewBox', '0 0 15 15');
+  logoSVG.setAttribute('id', 'logo');
+  document.getElementById('content').appendChild(logoSVG);
 
-const path = document.createElementNS('http://www.w3.org/2000/svg' ,'path');
-path.setAttribute("d" , "M4.457,11.9892,1,8V7H14V8l-3.4961,3.9891ZM3.9882,2.5a.5.5,0,0,0-1,0v.5671l-1.7969.3675a.25.25,0,1,0,.094.4911l1.7029-.2776v.5662l-1.75.0357a.25.25,0,0,0,0,.5l1.75.0357V5.998h1Zm9.5,1.5-7.5.2625V2.9951l7.594-1.0737a.5.5,0,0,0-.1881-.9822L5.9792,2.4555a.4963.4963,0,0,0-.991.0445v.2276l-.493.1009V3.18l.493-.08V4.2974l-.493.01v.4608L13.4882,5a.5.5,0,0,0,0-1ZM10,13H5v.5757h5Z"
-)
-logoSVG.appendChild(path);
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  path.setAttribute("d", "M4.457,11.9892,1,8V7H14V8l-3.4961,3.9891ZM3.9882,2.5a.5.5,0,0,0-1,0v.5671l-1.7969.3675a.25.25,0,1,0,.094.4911l1.7029-.2776v.5662l-1.75.0357a.25.25,0,0,0,0,.5l1.75.0357V5.998h1Zm9.5,1.5-7.5.2625V2.9951l7.594-1.0737a.5.5,0,0,0-.1881-.9822L5.9792,2.4555a.4963.4963,0,0,0-.991.0445v.2276l-.493.1009V3.18l.493-.08V4.2974l-.493.01v.4608L13.4882,5a.5.5,0,0,0,0-1ZM10,13H5v.5757h5Z"
+  )
+  logoSVG.appendChild(path);
 
 }
 
