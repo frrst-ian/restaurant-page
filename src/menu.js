78ae0ead3398ee7d6c0950c522eb69d56001de1b
frrst-ian <frrstxd@proton.me>
2024-07-12 15:14:08 +0800

    Add DIV elements to the menu page
    These DIVs will be the containers of the content

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,28 @@
 const menuPage = () => {
-    console.log('hi');
+    const menuContainer = document.createElement('div');
+    menuContainer.setAttribute('id', 'menu-container');
+
+    const div1 = document.createElement('div');
+    const div2 = document.createElement('div');
+    const div3 = document.createElement('div');
+
+    div1.classList.add('maindish-div');
+    div2.classList.add('maindish-div');
+    div3.classList.add('maindish-div');
+
+ 
+
+
+    document.getElementById('content').appendChild(menuContainer);
+    document.getElementById('menu-container').appendChild(div1);
+    document.getElementById('menu-container').appendChild(div2);
+    document.getElementById('menu-container').appendChild(div3);
+
+
+
+
+
+
 };
 
-export{menuPage}
+export { menuPage }
